7c7e2749f86c5a3de9e96a4df6fd15589ef087e3
EEnzo53 <etheveenzo00@gmail.com>
2026-06-23 15:54:16 +0200

    Add 9-print_comb.c to display the first 10 digits with commas

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Display the 9 first numbers and the 6 first letters
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int e;
+
+for (e = '0'; e <= '9'; e++)
+{
+    putchar(e);
+    if (e != '9')
+    {
+        putchar(',');
+        putchar(' ');
+    }
+}
+putchar('\n');
+	return (0);
+}
