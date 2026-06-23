aa7b3ac1a6e9df3c3e980d9bc0050821a381d8cc
EEnzo53 <etheveenzo00@gmail.com>
2026-06-23 11:39:07 +0200

    Update VSCode settings to include window zoom configuration

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,25 +1,28 @@
-	#include <stdlib.h>
-	#include <time.h>
-	/* main assign a random number to n  */
-
-	/* betty style doc for function main goes there */
-	int main(void)
-	{
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Choose a random number and say if it's negative or positive
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-	printf("%d is positive\n", n);
+		printf("%d is positive\n", n);
 	}
 	else if (n < 0)
 	{
-	printf("%d is negative\n" , n);
+		printf("%d is negative\n", n);
 	}
 	else
 	{
-    printf("%d is zero\n", n);
+		printf("%d is zero\n", n);
 	}
 	return (0);
 	}
