c0164989455fce91b1412923abaf4f2a943c7892
EEnzo53 <etheveenzo00@gmail.com>
2026-06-23 15:41:23 +0200

    Change /**

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Display all single digit numbers from 0 to 9 without use char
+ * main - Display the 9 first numbers and the 6 first letters
  *
  * Return: Always 0 (Success)
  */
