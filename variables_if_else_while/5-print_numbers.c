e22c1cd73764e57e61217d5d04aa99dc91a3d683
EEnzo53 <etheveenzo00@gmail.com>
2026-06-23 15:08:52 +0200

    Add 5-print_numbers.c to display all single digit numbers from 0 to 9

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Display all single digit numbers from 0 to 9
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+	{
+		putchar(d);
+	}
+	putchar('\n');
+	return (0);
+}
