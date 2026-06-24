a5013edacdf9ed8104b5b0305bbf6dcfec0480bb
EEnzo53 <etheveenzo00@gmail.com>
2026-06-24 13:56:12 +0200

    Add print_alphabet_x10 function to display the alphabet ten times

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * print_alphabet - Displays the alphabet ten times with a prototype
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int f;
+	for (f = 0; f <= 9; f++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	}
+	_putchar('\n');
+}
