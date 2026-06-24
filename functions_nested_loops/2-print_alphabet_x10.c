97f143ae7a943a2a7afcffffe94c0236495b5175
EEnzo53 <etheveenzo00@gmail.com>
2026-06-24 13:58:16 +0200

    Modif for betty

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include <unistd.h>
 #include "main.h"
 /**
- * print_alphabet - Displays the alphabet ten times with a prototype
+ * print_alphabet_x10 - Displays the alphabet ten times with a prototype
  *
  * Return: Always 0 (Success)
  */
@@ -10,9 +10,10 @@ void print_alphabet_x10(void)
 	char c;
 	int f;
 	for (f = 0; f <= 9; f++)
+
 	{
 		for (c = 'a'; c <= 'z'; c++)
 		_putchar(c);
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
