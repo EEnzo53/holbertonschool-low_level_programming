ec149e97079f17451ef87b972ecbdefc33cc989b
EEnzo53 <etheveenzo00@gmail.com>
2026-06-24 14:59:02 +0200

    Add _isalpha function to check for alphabetic characters

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _isalpha - Checs for alphabetic character
+ * @c: character to check
+ *
+ * Return 1 if is alphabetic character, otherwise 0
+ */
+int _isalpha(int c);
+{
+	if (c = ['a'-'z'] && c = ['A'-'Z'])
+		return (1),
+	return (0);
+}
