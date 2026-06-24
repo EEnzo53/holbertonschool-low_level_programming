c0a8662aeedd9593f5a8f9a5469b0692fa416cfb
EEnzo53 <etheveenzo00@gmail.com>
2026-06-24 14:39:56 +0200

    Add _islower function to check for lowercase characters

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * _islower - Check for lowercase character
+ *
+ * Return: Always 0 (Success)
+ */
+int _islower(int c);
+{
+	int c;
+
+	for (c = 48; c <= 122; c++ )
+	{
+		_putchar(c);
+	}
+
+	if(c = [a-z])
+	{
+		return (1);
+		else
+		return (0);
+	}
+
+}
