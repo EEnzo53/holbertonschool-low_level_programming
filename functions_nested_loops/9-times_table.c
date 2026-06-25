ff3cc43601702f122ea7a8aa2ada12e890035c4f
EEnzo53 <etheveenzo00@gmail.com>
2026-06-25 12:33:01 +0200

    DQV

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -12,7 +12,7 @@ void times_table(void)
 	{
 		for (ligne = 0; ligne <= 9; ligne++)
 		{
-			if (ligne * colonnes <= 57)
+			if (ligne * colonnes <= 9)
 			{
 				_putchar(ligne * colonnes + '0');
 				_putchar(',');
