a40528261be0948d99303449b0f08938ff3456f4
EEnzo53 <etheveenzo00@gmail.com>
2026-06-25 13:47:01 +0200

    fea

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,8 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *add - Additione 2 chiffre
- *
+ * add - Additione 2 chiffre
+ * a - chiffre 1
+ * b - chiffre 2
  * Return: affiche le résultat
  */
 int add(int a,int b)
