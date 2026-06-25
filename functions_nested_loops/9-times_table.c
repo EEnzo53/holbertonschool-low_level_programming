be1ea230186ef97cf8e0fe360c412ebad1059943
EEnzo53 <etheveenzo00@gmail.com>
2026-06-25 12:02:53 +0200

    Add 9-times_table.c

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * times_table - Display tables of multiplications
+ */
+void times_table(void)
+{
+	int ligne;
+	int colonnes;
+
+	for (ligne = 0; ligne <= 9; ligne++)
+	{
+		for (colonnes = 0; colonnes <= 9; colonnes++)
+		{
+			_putchar(ligne * colonnes + 0);
+			_putchar(,);
+			_putchar( );
+		}
+	}
+}
