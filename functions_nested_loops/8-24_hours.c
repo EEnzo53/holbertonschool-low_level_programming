a82d70b5f1dac261a62f184432714cce8bb99ed3
EEnzo53 <etheveenzo00@gmail.com>
2026-06-25 11:37:16 +0200

    Modif

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -8,9 +8,10 @@ void jack_bauer(void)
 	int hours;
 	int minutes;
 
-	for (minutes = 0; minutes <= 59; minutes++)
+	for (hours = 0; hours <= 23; hours++)
+
 	{
-		for (hours = 0; hours <= 23; hours++)
+		for (minutes = 0; minutes <= 59; minutes++)
 		{
 		_putchar(hours / 10 + '0');
 		_putchar(hours % 10 + '0');
