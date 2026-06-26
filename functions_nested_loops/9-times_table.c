129f87186b6cc8aa39007d33241e182382485335
EEnzo53 <etheveenzo00@gmail.com>
2026-06-26 13:07:50 +0200

    Modif completement les tables

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -5,24 +5,27 @@
  */
 void times_table(void)
 {
-	int ligne, colonnes;
+	int ligne, colonnes, result;
 
 	for (colonnes = 0; colonnes <= 9; colonnes++)
 	{
 		for (ligne = 0; ligne <= 9; ligne++)
+		result = (ligne * colonnes);
 		{
-			if (ligne * colonnes < 10 && colonnes != 0)
+			if (ligne * colonnes <= 10)
 			{
-				_putchar((ligne * colonnes) / 10 + '0');
-				_putchar((ligne * colonnes) % 10 + '0');
+				_putchar(result / 10 + '0');
 				_putchar(',');
 				_putchar(' ');
 			}
-			else if (ligne * colonnes < 10)
+			else if (ligne * colonnes >= 10)
 			{
-				_putchar(ligne * colonnes + '0');
+				_putchar(result % 10 + '0');
+				_putchar(',');
+				_putchar(' ');
 			}
 		}
 		_putchar('\n');
 	}
 }
+
