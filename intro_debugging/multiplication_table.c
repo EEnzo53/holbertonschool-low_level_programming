1eacf76e60ccce2e84244413e16401fe42547e5e
EEnzo53 <etheveenzo00@gmail.com>
2026-06-28 13:59:38 +0200

    qsc

--- intro_debugging/multiplication_table.c
+++ intro_debugging/multiplication_table.c
@@ -1,31 +1,25 @@
 #include <stdio.h>
-/**
- * print_table - print an size x size multiplication table
- * @size: the number of rows and columns of the table (must be > 0)
- *
- * Description: prints the multiplication table from 1 to size
- */
+
+
+
 void print_table(int size)
 {
 	int row, col;
 
 	for (row = 1; row <= size; row++)
 	{
-		for (col = 1; col <= size; col++)
+		for (col = 1; col <= size; col++) // Removed the extra increment
 		{
 			printf("%d ", row * col);
 		}
 		printf("\n");
 	}
-	return (0);
 }
 
-/**
- * main - Entry point. Display multiplication table of size 4
- *
- * Return: Always 0
- */
+/* -- DO NOT Modify the code below this line -- */
+
 int main(void)
 {
 	print_table(4);
+	return 0;
 }
