854629f19e9eec89b143c27b44e09e5a8b06ba47
EEnzo53 <etheveenzo00@gmail.com>
2026-06-29 14:28:49 +0200

    add comparison_algorithms-metrics.md: create a new file for benchmarking metrics

--- benchmarking/comparison_algorithms.c
+++ benchmarking/comparison_algorithms.c
@@ -7,73 +7,73 @@ static int data[ARRAY_SIZE];
 
 static void initialize_data(void)
 {
-    int i;
+	int i;
 
-    for (i = 0; i < ARRAY_SIZE; i++)
-        data[i] = i;
+	for (i = 0; i < ARRAY_SIZE; i++)
+		data[i] = i;
 }
 
-static int count_even_naive(void)
+	static int count_even_naive(void)
 {
-    int count;
-    int i;
-    int j;
+	int count;
+	int i;
+	int j;
 
-    count = 0;
+	count = 0;
 
-    for (i = 0; i < ARRAY_SIZE; i++)
-    {
-        for (j = 0; j <= i; j++)
-        {
-            if (data[j] % 2 == 0)
-                count++;
-        }
-    }
+	for (i = 0; i < ARRAY_SIZE; i++)
+	{
+		for (j = 0; j <= i; j++)
+		{
+			if (data[j] % 2 == 0)
+				count++;
+		}
+	}
 
-    return count;
+	return count;
 }
 
 static int count_even_single_pass(void)
 {
-    int count;
-    int i;
+	int count;
+	int i;
 
-    count = 0;
+	count = 0;
 
-    for (i = 0; i < ARRAY_SIZE; i++)
-    {
-        if (data[i] % 2 == 0)
-            count++;
-    }
+	for (i = 0; i < ARRAY_SIZE; i++)
+	{
+		if (data[i] % 2 == 0)
+			count++;
+	}
 
-    return count;
+	return count;
 }
 
 int main(void)
 {
-    clock_t start;
-    clock_t end;
-    double elapsed_a;
-    double elapsed_b;
-    int result_a;
-    int result_b;
+	clock_t start;
+	clock_t end;
+	double elapsed_a;
+	double elapsed_b;
+	int result_a;
+	int result_b;
 
-    initialize_data();
+	initialize_data();
 
-    start = clock();
-    result_a = count_even_naive();
-    end = clock();
-    elapsed_a = (double)(end - start) / (double)CLOCKS_PER_SEC;
+	start = clock();
+	result_a = count_even_naive();
+	end = clock();
+	elapsed_a = (double)(end - start) / (double)CLOCKS_PER_SEC;
 
-    start = clock();
-    result_b = count_even_single_pass();
-    end = clock();
-    elapsed_b = (double)(end - start) / (double)CLOCKS_PER_SEC;
+	start = clock();
+	result_b = count_even_single_pass();
+	end = clock();
+	elapsed_b = (double)(end - start) / (double)CLOCKS_PER_SEC;
 
-    printf("Naive algorithm result: %d\n", result_a);
-    printf("Naive algorithm time: %.6f seconds\n", elapsed_a);
-    printf("Single-pass algorithm result: %d\n", result_b);
-    printf("Single-pass algorithm time: %.6f seconds\n", elapsed_b);
+	printf("Naive algorithm result: %d\n", result_a);
+	printf("Naive algorithm time: %.6f seconds\n", elapsed_a);
+	printf("Single-pass algorithm result: %d\n", result_b);
+	printf("Single-pass algorithm time: %.6f seconds\n", elapsed_b);
 
-    return 0;
+	return (0);
 }
