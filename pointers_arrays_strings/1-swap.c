6db8cb2465fea1d95d3eaf081fa87464501348c0
EEnzo53 <etheveenzo00@gmail.com>
2026-06-30 14:15:50 +0200

    vdq

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -17,21 +17,4 @@ void swap_int(int *a, int *b)
 	*b = temp;
 }
 
-/**
- * main - entry point
- *
- * Return: 0 on success
- */
-int main(void)
-{
-	int n = 78;
-	int m = 15;
-	int *paprika = &n;
 
-	paprika = &m;
-	*paprika = 98;
-	paprika = &n;
-	*paprika = 94;
-
-	return (0);
-}
