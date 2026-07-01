678f10f2a366a1743df67b50eb9e24e421135800
EEnzo53 <etheveenzo00@gmail.com>
2026-07-01 09:39:35 +0200

    Improve formatting and add documentation for rev_string function

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,5 +1,8 @@
 #include "main.h"
-
+/**
+ * rev_string - Reverses a string.
+ * @s: The string to reverse.
+ */
 void rev_string(char *s)
 {
 	int i;
@@ -9,12 +12,12 @@ void rev_string(char *s)
 	i = 0;
 	j = _strlen(s) - 1;
 
-	while(i < j)
-    {
+	while (i < j)
+	{
 		tmp = s[i];
 		s[i] = s[j];
 		s[j] = tmp;
 		i++;
 		j--;
-    }
+	}
 }
