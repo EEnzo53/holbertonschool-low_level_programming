588afa9872eaf8894861de6382569c41aa01b557
EEnzo53 <etheveenzo00@gmail.com>
2026-07-01 10:49:41 +0200

    htes

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -11,13 +11,12 @@ void puts_half(char *str)
 
 	len = 0;
 
-
 	while (str[len] != '\0')
 		len++;
 
 	i = (len + 1) / 2;
 
-	while (str[i] != '0')
+	while (str[i] != '\0')
 	{
 		_putchar(str[i]);
 		i++;
