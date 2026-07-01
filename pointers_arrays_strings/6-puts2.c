faa6e99a68beda8e3392c1261b717ae1cab408df
EEnzo53 <etheveenzo00@gmail.com>
2026-07-01 16:59:21 +0200

    sc

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,19 +1,22 @@
 #include "main.h"
-
 /**
  * puts2 - prints every other character of a string
- * @str: string to print
- */
+ *
+ * @str: the string to print
+*/
 
 void puts2(char *str)
 {
-	int i = 0;
+	int i;
+	int len;
 
-	while (str[i] != '\0')
-	{
+	len = 0;
+
+	while (str[len] != '\0')
+		len++;
+
+	for (i = 0; i < len; i += 2)
 		_putchar(str[i]);
-		i += 2;
-	}
 
 	_putchar('\n');
 }
