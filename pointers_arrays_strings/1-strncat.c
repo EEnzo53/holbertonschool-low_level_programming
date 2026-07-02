3a54aa22b0956628d72cf19660cc18e2ff58f7d5
EEnzo53 <etheveenzo00@gmail.com>
2026-07-02 10:27:49 +0200

    Refactor _strncat function documentation for clarity and consistency

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,8 +1,11 @@
 #include "main.h"
 /**
- * _strncat - Add dest in src with a spec charactrs
+ * _strncat - concatenates two strings
+ * @dest: destination string
+ * @src: source string
+ * @n: maximum number of bytes to use from src
  *
- * Return: always dest
+ * Return: pointer to destination string
  */
 
 char *_strncat(char *dest, char *src, int n)
@@ -10,7 +13,7 @@ char *_strncat(char *dest, char *src, int n)
 	int i = 0;
 	int len = 0;
 
-	while(dest[len] != '\0')
+	while (dest[len] != '\0')
 	{
 		len++;
 	}
