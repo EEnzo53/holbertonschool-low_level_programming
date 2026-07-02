07221c44efa46b106eb7704c36d7de03a9e286eb
EEnzo53 <etheveenzo00@gmail.com>
2026-07-02 11:58:23 +0200

    Fix _strcmp function logic and improve documentation

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,11 +1,17 @@
 #include "main.h"
 
-
+/**
+ * _strcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: 0 if equal, negative if s1 < s2, positive if s1 > s2
+ */
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
 
-	while (s1[i] == s2[i] && s1[i != '\0'])
+	while (s1[i] == s2[i] && s1[i] != '\0'])
 		i++;
 	return (s1[i] - s2[i]);
 
