dcad1eddcc00ec40c989b41772ab81a825a7b4a0
EEnzo53 <etheveenzo00@gmail.com>
2026-07-02 11:59:29 +0200

    Rm a ]

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _strcmp - compares two strings
  * @s1: first string
@@ -11,7 +10,7 @@ int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
 
-	while (s1[i] == s2[i] && s1[i] != '\0'])
+	while (s1[i] == s2[i] && s1[i] != '\0')
 		i++;
 	return (s1[i] - s2[i]);
 
