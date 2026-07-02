a4ca0c96724aece976c4232bb1ee135662334db8
EEnzo53 <etheveenzo00@gmail.com>
2026-07-02 14:30:17 +0200

    Fix string_toupper function to handle NULL input and improve documentation

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -3,12 +3,16 @@
  * sring_toupper - changes all lowercase letters of a string to uppercase.
  *
  */
-char *string_toupper(char *)
+char *string_toupper(char *str)
 {
-	int i;
+	int i = 0;
 
 	while (str[i] != '\0')
+	{
 		if (str[i] >= 'a' && str[i] <= 'z')
 			str[i] = str[i] - 32;
 		i++;
+	}
+
+	return (str);
 }
