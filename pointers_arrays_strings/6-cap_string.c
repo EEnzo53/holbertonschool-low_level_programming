ef3039e00955721a39c5c91ac273ae65df9fe4dc
EEnzo53 <etheveenzo00@gmail.com>
2026-07-02 14:44:25 +0200

    Betty

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,40 +1,40 @@
 #include "main.h"
 
 /**
- * cap_string - capitalizes all words of a string
- * @str: the string to modify
- *
- * Return: pointer to the modified string
- */
+* cap_string - capitalizes all words of a string
+* @str: the string to modify
+*
+* Return: pointer to the modified string
+*/
 
 char *cap_string(char *str)
 {
-    int i = 0;
+	int i = 0;
 
-    while (str[i] != '\0')
-    {
-        if (str[i] >= 'a' && str[i] <= 'z')
-        {
-            if (i == 0 ||
-                str[i - 1] == ' '  ||
-                str[i - 1] == '\t' ||
-                str[i - 1] == '\n' ||
-                str[i - 1] == ','  ||
-                str[i - 1] == ';'  ||
-                str[i - 1] == '.'  ||
-                str[i - 1] == '!'  ||
-                str[i - 1] == '?'  ||
-                str[i - 1] == '"'  ||
-                str[i - 1] == '('  ||
-                str[i - 1] == ')'  ||
-                str[i - 1] == '{'  ||
-                str[i - 1] == '}')
-            {
-                str[i] = str[i] - 32;
-            }
-        }
-        i++;
-    }
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			if (i == 0 ||
+				str[i - 1] == ' '  ||
+				str[i - 1] == '\t' ||
+				str[i - 1] == '\n' ||
+				str[i - 1] == ','  ||
+				str[i - 1] == ';'  ||
+				str[i - 1] == '.'  ||
+				str[i - 1] == '!'  ||
+				str[i - 1] == '?'  ||
+				str[i - 1] == '"'  ||
+				str[i - 1] == '('  ||
+				str[i - 1] == ')'  ||
+				str[i - 1] == '{'  ||
+				str[i - 1] == '}')
+			{
+				str[i] = str[i] - 32;
+			}
+		}
+		i++;
+	}
 
-    return (str);
+	return (str);
 }
