1243b191a5866a8746d63b3e4788682b3194199a
EEnzo53 <etheveenzo00@gmail.com>
2026-07-02 14:03:18 +0200

    Improve documentation for reverse_array function parameters

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,6 +1,8 @@
 #include "main.h"
 /**
  * reverse_array - reverses the content of an array of integers.
+ * @a: pointer to the array of integers.
+ * @n: number of elements in the array.
  */
 
 void reverse_array(int *a, int n)
