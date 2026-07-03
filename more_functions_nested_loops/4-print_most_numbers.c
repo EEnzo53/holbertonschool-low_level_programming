f3deeccb4472cb6f0b8a62f743cadc23aed5398d
EEnzo53 <etheveenzo00@gmail.com>
2026-07-03 10:33:48 +0200

    Update documentation for print_most_numbers function to clarify its purpose

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * print_numbers - print number 0 to 9
+ * print_most_numbers - prints the numbers 0 to 9 excluding 2 and 4
+ *
+ * Return: void
  */
 
 void print_most_numbers(void)
