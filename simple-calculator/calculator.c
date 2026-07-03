8ad96376435c913920bb3d3c40516ad2170bc990
EEnzo53 <etheveenzo00@gmail.com>
2026-07-03 16:55:28 +0200

    Fix typos and improve formatting in calculator program

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -1,23 +1,20 @@
 #include <stdio.h>
-
+/**
+ * main - entry point for simple calculator program
+ *
+ * Return: 0 on success
+ */
 int main(void)
 {
-    int choice = -1;
-
-//	int a = 0;
-//	int b = 0;
-//	int result = 0;
+	int choice = -1;
 
 	printf("simple calculator\n");
 	while (choice != 0)
 	{
-		printf("1) add\n2) Substact\n3) Multiply\n4) Divide\n0) Quit\n");
-		printf("choice ?\n")
+		printf("1) add\n2) Subtract\n3) Multiply\n4) Divide\n0) Quit\n");
+		printf("choice ?\n");
 		scanf("%d", &choice);
-		//if (choice < 0 || choice > 4)
-        //{
-
-       // }
 	}
 
+	return (0);
 }
