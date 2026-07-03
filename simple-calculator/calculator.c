b4bd5affba000867cf3a54d6fd8c4766a29b1780
EEnzo53 <etheveenzo00@gmail.com>
2026-07-03 09:13:09 +0200

    Add initial implementation of calculator and memory set functions

---
+++ simple-calculator/calculator.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int choice = -1;
+    int a = 0;
+    int b = 0;
+    int result = 0;
+
+    printf("Welcome to the CALCULATOOOR");
+    while (choice != 0)
+    {
+
+    }
+
+}
