27bf172c9a907aa6ab7fc1f10e77502fe035b00e
EEnzo53 <etheveenzo00@gmail.com>
2026-07-09 14:43:05 +0200

    Refactor new_dog function to use malloc instead of strdup for name and owner

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,5 +1,4 @@
 #include <stdlib.h>
-#include <string.h>
 #include "dog.h"
 
 /**
@@ -13,27 +12,43 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
+	int i;
 
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
 
-	dog->name = strdup(name);
+	/* Copier le nom */
+	dog->name = malloc(sizeof(char) * (strlen(name) + 1));
 	if (dog->name == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
 
+	for (i = 0; name[i] != '\0'; i++)
+	{
+		dog->name[i] = name[i];
+	}
+	dog->name[i] = '\0';
+
+	/* Copier l'âge */
 	dog->age = age;
 
-	dog->owner = strdup(owner);
+	/* Copier le propriétaire */
+	dog->owner = malloc(sizeof(char) * (strlen(owner) + 1));
 	if (dog->owner == NULL)
 	{
 		free(dog->name);
 		free(dog);
 		return (NULL);
 	}
+
+	for (i = 0; owner[i] != '\0'; i++)
+	{
+		dog->owner[i] = owner[i];
+	}
+	dog->owner[i] = '\0';
 
 	return (dog);
 }
