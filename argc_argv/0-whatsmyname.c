2c57d20c4a99b8275913a7ab84867c9424262c16
EEnzo53 <etheveenzo00@gmail.com>
2026-07-15 10:49:05 +0200

    Add program to display its own name using argc and argv

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Display name of prog
+ *@argv: name to display
+ *@argc: Nothing here
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
