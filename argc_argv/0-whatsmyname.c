5084408d6b69bf7354b9d92b7bcfb19984159821
EEnzo53 <etheveenzo00@gmail.com>
2026-07-15 10:53:15 +0200

    Fix main function signature to include argc parameter

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
  * main - Display name of prog
- *@argv: name to display
+ * @argc: number of command line arguments
+ * @argv: array of command line arguments
  *
  * Return: Always 0
  */
-int main(char *argv[])
+int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
