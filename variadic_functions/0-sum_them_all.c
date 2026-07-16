486e0f9c88a2a17717eaa5673ba8111ece0e1a2a
EEnzo53 <etheveenzo00@gmail.com>
2026-07-16 11:44:52 +0200

    Add variadic function to sum all arguments and update headers

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include <stdarg.h>
+
+/**
+ * sum_them_all - Sums all the arguments passed to the function
+ * @n: The number of arguments passed
+ * ...: Variable number of integers to sum
+ *
+ * Return: The sum of all arguments
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum = 0;
+	int i;
+	va_list args;
+
+	if (n == 0)
+		return (0);
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(args, int);
+	}
+	va_end(args);
+	return (sum);
+}
