6c71dfac5360d3a86773667f35d1e13343578356
EEnzo53 <etheveenzo00@gmail.com>
2026-07-20 12:12:43 +0200

    Refactor _print_rev_recursion function to remove unnecessary code and improve clarity

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,18 +1,16 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * _puts_recursion - prints a string in reverse, followed by a new line.
+ * _print_rev_recursion - prints a string in reverse, followed by a new line.
  * @s: pointer to the string to print.
  */
-void _print_rev_recursion(char *s);
+void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		_putchar('\n');
 		return;
 	}
 
+	_print_rev_recursion(s + 1);
 	_putchar(*s);
-	_puts_recursion(s - 1);
 }
