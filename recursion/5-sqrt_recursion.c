4fc3e8f7126dc030c3f7d6a56e60aec677b3d123
EEnzo53 <etheveenzo00@gmail.com>
2026-07-20 12:57:32 +0200

    Add _sqrt_recursion function to calculate the natural square root of a number

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _sqrt_helper - recursive helper to find a square root
+ * @n: number to calculate the square root of
+ * @i: current candidate root
+ *
+ * Return: natural square root or -1 if not found
+ */
+static int _sqrt_helper(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (_sqrt_helper(n, i + 1));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number to calculate the square root of
+ *
+ * Return: natural square root or -1 if n has no natural square root
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	return (_sqrt_helper(n, 0));
+}
