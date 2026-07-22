2509db97fa911dfe4dd07273b6091886cf51074a
EEnzo53 <etheveenzo00@gmail.com>
2026-07-22 09:54:50 +0200

    Add implementation of print_list function and header file for singly linked list

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer to the head of the list
+ *
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	int nodes = 0;
+	int *ptr = h;
+
+	while (*ptr != NULL)
+	{
+		if (ptr->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%u] %s\n", ptr->len, ptr->str);
+		}
+		ptr = ptr->next;
+		nodes++;
+	}
+	return (nodes);
+}
---
+++ singly_linked_lists/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+* struct list_s - Singly linked list node
+* @str: String data
+* @len: Length of the string
+* @next: Pointer to the next node
+*/
+typedef struct list_s
+	{
+		char *str;
+
+		unsigned int len;
+
+		struct list_s *next;
+	} list_t;
+
+size_t print_list(const list_t *h);
+
+#endif
