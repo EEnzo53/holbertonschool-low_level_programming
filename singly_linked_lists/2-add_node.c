9cca767c539593bed996605d88904c1f491a19a3
EEnzo53 <etheveenzo00@gmail.com>
2026-07-22 15:30:20 +0200

    Fix add_node function to ensure new_node is allocated before usage

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -17,17 +17,19 @@ list_t *add_node(list_t **head, const char *str)
 	while (str[len] != '\0')
 	{
 		len++;
-		if (new_node == NULL)
-			return (NULL);
-
-		new_node->str = strdup(str);
-		if (new_node->str == NULL)
-		{
-			free(new_node);
-			return (NULL);
-		}
-		new_node->next = *head;
-		*head = new_node;
 	}
+
+	new_node->str = strdup(str);
+
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->len = len;
+	new_node->next = *head;
+	*head = new_node;
+
 	return (new_node);
 }
