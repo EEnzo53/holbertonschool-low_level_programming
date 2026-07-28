fcb3da7d3f9cfd1515d9e068923a7839d6b7698a
EEnzo53 <etheveenzo00@gmail.com>
2026-07-28 10:19:36 +0200

    Refactor add_dnodeint_end function for improved readability and structure

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -3,27 +3,38 @@
 #include "lists.h"
 
 /**
- * add_dnodeint_end - adds a node at the end of a doubly linked list
- * @head: pointer to the head pointer of the list
- * @n: integer value to store in the new node
- *
- * Return: address of the new node, or NULL on failure
- */
+* add_dnodeint_end - adds a node at the end of a doubly linked list
+* @head: pointer to the head pointer of the list
+* @n: integer value to store in the new node
+*
+* Return: address of the new node, or NULL on failure
+*/
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *nodes = malloc(sizeof(dlistint_t));
+	dlistint_t *nodes;
+	dlistint_t *tmp;
 
+	nodes = malloc(sizeof(dlistint_t));
 	if (nodes == NULL)
 		return (NULL);
 
 	nodes->n = n;
-	nodes->prev = *head;
 	nodes->next = NULL;
 
-	if (*head != NULL)
-		(*head)->next = nodes;
+	if (*head == NULL)
+	{
+		nodes->prev = NULL;
+		*head = nodes;
+		return (nodes);
+	}
 
-	*head = nodes;
+	tmp = *head;
+
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+
+	tmp->next = nodes;
+	nodes->prev = tmp;
 
 	return (nodes);
 }
