ae3619f98cf6ab771773cdd66f02336dc6e75b2e
EEnzo53 <etheveenzo00@gmail.com>
2026-07-28 09:14:41 +0200

    Add print_dlistint function and main.h header for doubly linked list

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_dlistint - prints all elements of a doubly linked list
+ * @h: pointer to the head of the list
+ *
+ * Return: the number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t nodes = 0;
+	const dlistint_t *tmp = h;
+
+	while (tmp != NULL)
+	{
+		printf("%d\n", tmp->n);
+		tmp = tmp->next;
+		nodes++;
+	}
+	return (nodes);
+}
