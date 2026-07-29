321489a13cc1d447a79e5af33e55b9b3042b2111
EEnzo53 <etheveenzo00@gmail.com>
2026-07-29 14:37:36 +0200

    Fix include directive and improve documentation in hash_table_set function

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,24 +1,42 @@
-#include "hash_tables.h"
+#include "main.h"
 #include <stdlib.h>
 #include <string.h>
 
 /**
- * hash_table_set - adds an element to a hash table
+ * hash_table_set - adds an element to the hash table
  * @ht: hash table
- * @key: key of the element
- * @value: value associated with the key
+ * @key: key
+ * @value: value associated with key
  *
- * Return: 1 if it succeeded, 0 otherwise
+ * Return: 1 if success, 0 otherwise
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new_node;
+	hash_node_t *current;
 	unsigned long int index;
 
 	if (ht == NULL || key == NULL || key[0] == '\0')
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
+	current = ht->array[index];
+
+	while (current)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			free(current->value);
+			current->value = strdup(value);
+
+			if (current->value == NULL)
+				return (0);
+
+			return (1);
+		}
+
+		current = current->next;
+	}
 
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
